ff89f39167497d852f8edad606b57bf73726c7bc
Reo Tang <reot@colossus.it.mtu.edu>
2020-12-15 00:48:08 -0500

    correct/incorrect choice

--- CreateEntireExam.php
+++ CreateEntireExam.php
@@ -13,7 +13,11 @@
             echo "<input type='hidden' name='question".$x."points' value=".$_POST["question".$x."points"].">";
             for($y=1; $y <= $_POST["question".$x."choices"]; $y++) {
               echo "<label for='question".$x."choice".$y."'>Choice".$y."</label>";
-              echo "<input type='text' name='question".$x."choice".$y."'><br>";
+              echo "<input type='text' name='question".$x."choice".$y."'>";
+              echo "<label for='correct'> Correct</label>";
+              echo "<input type='radio' id='correct' name='question".$x."choice".$y."correctness' value='correct'>";
+              echo "<label for='correct'> Incorrect</label>";
+              echo "<input type='radio' id='incorrect' name='question".$x."choice".$y."correctness' value='incorrect'><br>";
             }
           }
           echo "<input type='submit' value='Create Exam'>";
